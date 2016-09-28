daea54c987aaf1c57f16759e0fe6cd107d4aa1d2
Joey Mudd <joseph.mudd@gmail.com>
2016-09-28 09:52:03 -0400

    Add a simple modal to display individual characters when clicked

--- Solutions/Week5/js/app.js
+++ Solutions/Week5/js/app.js
@@ -1,48 +1,56 @@
-var prevUrl = '', 
+var prevUrl = '',
     nextUrl = '',
     initialUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=20';
 
-function addPokemon(name) {
+function addPokemon(name,url) {
     $(`
         <li class="poke-card">
             <h3 class="name">${name}</h3>
         </li>
-    `).appendTo('#pokemon');
+    `).appendTo('#pokemon').click(function(){
+      getPokemonDetail(url);
+    });
 };
 
+function getPokemonDetail(url){
+  $.getJSON(url,function(data){
+    var detail = `<img src="${data.sprites.front_default}"><div>${data.name}</div>`;
+    $('#detailModal').html(detail).dialog({modal: true});
+    console.log(data);
+  });
+}
 
 // 1.)  Use the PokéAPI from http://pokeapi.co along with jQuery's getJSON function to retrieve the first 20 Pokémon.
-// 1.1)  Use the addPokemon function to show each of the Pokémon names that were retrieved.  
+// 1.1)  Use the addPokemon function to show each of the Pokémon names that were retrieved.
 //Hint: Learn how to access resources via the documentation http://pokeapi.co/docsv2/#resource-lists
 
 function getPokemon(url){
   $.getJSON(url,function(data){
-    console.log(data);
     prevUrl = data.previous;
     nextUrl = data.next;
-    
+
     if(prevUrl === null){
-      $('#previous').hide(); 
-    } else{ 
-      $('#previous').show(); 
+      $('#previous').hide();
+    } else{
+      $('#previous').show();
     }
-    
-    if(nextUrl === null){ 
+
+    if(nextUrl === null){
       $('#next').hide();
     } else{
       $('#next').show();
     }
-    
+
     $(data.results).each(function(key,item){
-      addPokemon(item.name);
+      addPokemon(item.name,item.url);
     });
   });
 }
 
 // 2.)  Use jQuery to create a click handler for the next and previous buttons.
 // 2.1) Use the "next" and "previous" properties of the pokemon resource object to get the next or previous list of Pokémon.
-// 2.2) When a user clicks next or previous, remove all existing Pokémon from the ul element and add the new list of Pokémon.  
-// 2.3) Use jQuery to disable the next/previous buttons if there are no more Pokémon to retrieve in that direction.  
+// 2.2) When a user clicks next or previous, remove all existing Pokémon from the ul element and add the new list of Pokémon.
+// 2.3) Use jQuery to disable the next/previous buttons if there are no more Pokémon to retrieve in that direction.
 
 $('#previous').click(function(){
   $('#pokemon').html('');
@@ -58,7 +66,7 @@ $('#next').click(function(){
     When you click on a Pokémon name, hide all the names and show a larger card that contains details about that Pokémon such as their sprite (picture), name,
     type or anything else you would like to include.  Add a way to go back to the list when your're done looking at the detail.
 
-    Be creative, you can style/arrange the detail information however you like! 
+    Be creative, you can style/arrange the detail information however you like!
 */
 
 getPokemon(initialUrl);
